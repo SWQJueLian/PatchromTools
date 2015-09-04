479f1c26c119f6aa3a373c3b48271c6082d2d4ac
绝恋。。 <1105033640@qq.com>
2015-09-04 13:09:50 +0800

    Update autoSync.sh

--- autoSync.sh
+++ autoSync.sh
@@ -2,11 +2,11 @@
 
 echo "------- Begin autoSync Source Code -----"
 
-~/bin/repo sync -c -j20
+repo sync -c -j20
 
 while [ $? -ne 0 ]
 do
 	echo "----- failed, Try again ------"
 	#-f, --force-broken    continue sync even if a project fails to sync
-	~/bin/repo sync -c -j20
+	repo sync -f -c -j20
 done
